7c22bf30f6142d9dd77949588c50e9bdda3801ef
kastona <stephenkastona@gmail.com>
2021-03-17 15:11:14 +0100

    sets yes to user experience

--- src/util/email.js
+++ src/util/email.js
@@ -178,7 +178,7 @@ module.exports.send = async (feedback) => {
         `                    </tr>\n` +                   
         `                    <tr>\n` +
         `                        <td bgcolor="#dee3f9" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: \`Lato\`, Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">\n` +
-        `                            <p style="margin: 5px; font-style: italic;">${feedback[0].radioModel? feedback[0].answer: 'User did not repond'}</p>\n` +
+        `                            <p style="margin: 5px; font-style: italic;">${feedback[0].radioModel? 'Yes. ' + feedback[0].answer: 'User did not repond'}</p>\n` +
         `                        </td>\n` +
         `                    </tr>\n` +
         `                </table>\n` +
